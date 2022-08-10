59cf538603d5c781af3a21642a1f5fb3e6f0e268
VasiliyMalyshev <Newaldan92@gmail.com>
2022-08-10 09:23:57 +0300

    a

---
+++ Microservice/src/main/java/com/malyshev/microservice/service/TinkoffStockService.java
@@ -0,0 +1,10 @@
+package com.malyshev.microservice.service;
+
+import com.malyshev.microservice.model.Stock;
+
+public class TinkoffStockService implements StockService {
+    @Override
+    public Stock getStockByTicker(String ticker) {
+        return null;
+    }
+}
